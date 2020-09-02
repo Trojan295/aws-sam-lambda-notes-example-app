fea4a6689f6336a70ac4dff7c91e7b25a4254543
Damian Czaja <trojan295@gmail.com>
2020-09-02 15:57:29 +0200

    fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,6 @@
   <div id="app">
     <div class="container">
       <div class="row">
-        <div class="one-half column"><p /></div>
-        <div class="one-half column">
-          <h5>Hello, {{ username }}</h5>
-        </div>
-      </div>
-      <div class="row">
         <AddNote />
       </div>
       <div class="row">
@@ -30,15 +24,5 @@ export default {
     NotesGrid,
     AddNote,
   },
-
-  computed: {
-    username() {
-      const payload = jwt.decode(this.$store.state.accessToken)
-      if (payload === null) {
-        return ''
-      }
-      return payload.email
-    }
-  }
 }
 </script>
